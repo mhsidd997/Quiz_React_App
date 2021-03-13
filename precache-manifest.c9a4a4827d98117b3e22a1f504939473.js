d381883bc7a34e03bb71cd89f4279d2020a8d289
mhsidd997 <muhammedhamzasiddiqui@gmail.com>
2021-03-14 00:38:39 +0500

    Updates

---
+++ precache-manifest.c9a4a4827d98117b3e22a1f504939473.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d726f7e573c00996ee33f7a586192a",
+    "url": "/Quiz_React_App/index.html"
+  },
+  {
+    "revision": "5f83468fbdd90582ce62",
+    "url": "/Quiz_React_App/static/css/main.a97a87af.chunk.css"
+  },
+  {
+    "revision": "092053f70763ba60f297",
+    "url": "/Quiz_React_App/static/js/2.5d05c98f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quiz_React_App/static/js/2.5d05c98f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f83468fbdd90582ce62",
+    "url": "/Quiz_React_App/static/js/main.e7c072d1.chunk.js"
+  },
+  {
+    "revision": "6f0ca401a0e8ed6ad20a",
+    "url": "/Quiz_React_App/static/js/runtime-main.b3700ad2.js"
+  }
+]);
